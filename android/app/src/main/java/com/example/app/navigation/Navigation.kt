42db6bf2f3df1da2a02ef58abbbabc9e13a3cb9e
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-10-24 15:27:25 +0300

    Create classes for each menu item, remove unncessary imports and add coil import

--- android/app/src/main/java/com/example/app/navigation/Navigation.kt
+++ android/app/src/main/java/com/example/app/navigation/Navigation.kt
@@ -3,10 +3,7 @@ package com.example.app.navigation
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
@@ -16,22 +13,17 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -41,7 +33,12 @@ import com.example.app.appbar.AppBar
 import com.example.app.drawer.DrawerBody
 import com.example.app.drawer.DrawerHeader
 import com.example.app.drawer.MenuItem
+import com.example.app.views.AboutScreen
+import com.example.app.views.DeliveriesScreen
+import com.example.app.views.HomeScreen
 import com.example.app.views.LoginForm
+import com.example.app.views.OrdersScreen
+import com.example.app.views.ProductsScreen
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 
@@ -106,7 +103,7 @@ fun Navigation() {
             }
         ){
             MainContent(navController)
-            ProductsSaleScreen()
+            ProductsScreen()
         }
         composable(
             route = Routes.deliveries,
@@ -268,34 +265,4 @@ fun HideSystemUi(){
 @Composable
 fun LoginScreen(navController: NavController) {
     LoginForm(navController)
-}
-
-@Composable
-fun HomeScreen() {
-
-}
-
-@Composable
-fun OrdersScreen() {
-
-}
-
-@Composable
-fun ProductsScreen() {
-
-}
-
-@Composable
-fun ProductsSaleScreen() {
-
-}
-
-@Composable
-fun DeliveriesScreen() {
-
-}
-
-@Composable
-fun AboutScreen() {
-
 }
