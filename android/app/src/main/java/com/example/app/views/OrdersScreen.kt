24dd9e2c3bc33bf6dc4f824085de57e678d4c630
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-11-06 12:49:26 +0200

    Implement loading spinner for orders page

--- android/app/src/main/java/com/example/app/views/OrdersScreen.kt
+++ android/app/src/main/java/com/example/app/views/OrdersScreen.kt
@@ -1,13 +1,17 @@
 package com.example.app.views
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -16,14 +20,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.app.model.MusicalInstruments
-import com.example.app.service.MusicalInstrumentsService
 import com.example.app.service.OrdersService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import utils.OrdersCardElement
-import utils.ProductsCardElement
 
 @Composable
 fun OrdersScreen(ordersService: OrdersService, navController: NavController) {
@@ -31,23 +29,37 @@ fun OrdersScreen(ordersService: OrdersService, navController: NavController) {
         ordersService.allOrders
     }.collectAsState(initial = emptyList())
 
-    LazyColumn {
-        items(ordersList) { order ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(vertical = 25.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                OrdersCardElement(
-                    orderID = order.orderID,
-                    orderedAt = order.orderedAt,
-                    quantity = order.quantity,
-                    totalPrice = order.totalPrice,
-                    navController = navController
-                )
+    if(ordersList.isEmpty()){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+    else {
+        LazyColumn {
+            items(ordersList) { order ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(vertical = 25.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    OrdersCardElement(
+                        orderID = order.orderID,
+                        orderedAt = order.orderedAt,
+                        quantity = order.quantity,
+                        totalPrice = order.totalPrice,
+                        navController = navController
+                    )
+                }
             }
         }
     }
